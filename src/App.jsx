be75cf59bee1e027a9ea9667b92aedf1a71d643b
JPS4L4 <juansalabet1@gmail.com>
2024-02-14 19:12:29 -0500

    Dia 1

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useFetch } from "./hooks/useFetch";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  console.log("App");
+
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/userss"
+  );
+
+  if (loading) {
+    return <div>Cargando</div>;
+  }
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <>
+      <h1>UseEffect</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter: {counter}
+      </button>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
